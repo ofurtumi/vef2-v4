f1a5686660c1ee86b2a3f84976aa067c53ab82d2
Tumi <thorvaldur.tumi@gmail.com>
2022-03-26 16:54:32 +0000

    veit ekki afh eslint krassar öllu en nuna virkar greinilega hahaha

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,43 @@
-module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: [
-		'airbnb-base',
-		'prettier',
-		'plugin:react/recommended',
-		'plugin:react-hooks/recommended',
-		'plugin:jsx-a11y/recommended',
-	],
-	parserOptions: {
-		ecmaVersion: 12,
-		sourceType: 'module',
-		jsx: true,
-	},
-	rules: {
-		'no-restricted-syntax': 0,
-		'import/prefer-default-export': 0,
-		'import/no-extraneous-dependencies': 0,
-		quotes: ['error', 'single'],
-		'no-console': [
-			'warn',
-			{
-				allow: [
-					'warn',
-					'error',
-					'info',
-					'group',
-					'groupCollapsed',
-					'groupEnd',
-				],
-			},
-		],
-		'import/extensions': 0,
-		'max-len': [
-			'warn',
-			{
-				code: 100,
-				ignoreUrls: true,
-				ignoreComments: true,
-				ignoreTemplateLiterals: true,
-			},
-		],
-	},
-};
+// module.exports = {
+// 	env: {
+// 		browser: true,
+// 		es2021: true,
+// 	},
+// 	extends: [
+// 		'airbnb-base',
+// 		'prettier',
+// 	],
+// 	parserOptions: {
+// 		ecmaVersion: 12,
+// 		sourceType: 'module',
+// 	},
+// 	rules: {
+// 		'no-restricted-syntax': 0,
+// 		'import/prefer-default-export': 0,
+// 		'import/no-extraneous-dependencies': 0,
+// 		quotes: ['error', 'single'],
+// 		'no-console': [
+// 			'warn',
+// 			{
+// 				allow: [
+// 					'warn',
+// 					'error',
+// 					'info',
+// 					'group',
+// 					'groupCollapsed',
+// 					'groupEnd',
+// 				],
+// 			},
+// 		],
+// 		'import/extensions': 0,
+// 		'max-len': [
+// 			'warn',
+// 			{
+// 				code: 100,
+// 				ignoreUrls: true,
+// 				ignoreComments: true,
+// 				ignoreTemplateLiterals: true,
+// 			},
+// 		],
+// 	},
+// };
